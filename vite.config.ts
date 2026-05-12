71d0f13ef314225b2aa8cc33e26eedaf6f3b68d1
dangdang2026 <qinyang2015@icloud.com>
2026-05-12 12:02:24 +0800

    部署: 输出目录 dist -> docs, 适配GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   base: './',
   build: {
     sourcemap: 'hidden',
-    outDir: 'dist',
+    outDir: 'docs',
     assetsInlineLimit: 100000000,
     cssCodeSplit: false,
     chunkSizeWarningLimit: 100000000,
